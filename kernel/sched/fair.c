2d511ee87313037f060856db02a2111597b79ffb
Paul Keith <javelinanddart@gmail.com>
2015-11-29 18:05:47 -0800

    sched: Export Arch Power to Sysfs
    * Based on @ktoonsez's Gentle Fair Sleepers Sysfs
    * Clean up some sysfs variable naming
    
    Change-Id: I0ec22bbae9a609535542a5b8c0674f9482280d74
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3709,9 +3709,14 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 }
 
 static unsigned int Lgentle_fair_sleepers = 1;
+static unsigned int Larch_power = 0;
 void relay_gfs(unsigned int gfs)
 {
 	Lgentle_fair_sleepers = gfs;
+}
+void relay_ap(unsigned int ap)
+{
+	Larch_power = ap;
 }
 
 static void
@@ -6559,7 +6564,7 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 	struct sched_group *sdg = sd->groups;
 
 	if ((sd->flags & SD_SHARE_CPUPOWER) && weight > 1) {
-		if (sched_feat(ARCH_POWER))
+		if (Larch_power)
 			power *= arch_scale_smt_power(sd, cpu);
 		else
 			power *= default_scale_smt_power(sd, cpu);
@@ -6569,7 +6574,7 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 
 	sdg->sgp->power_orig = power;
 
-	if (sched_feat(ARCH_POWER))
+	if (Larch_power)
 		power *= arch_scale_freq_power(sd, cpu);
 	else
 		power *= default_scale_freq_power(sd, cpu);
